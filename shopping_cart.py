0ad9fc422d1c43b88e55f09e02efccc0326efd98
araleigh1 <65408874+araleigh1@users.noreply.github.com>
2020-06-07 17:08:32 -0400

    Incorporated CSV and added time of purcase + message at end

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,57 +1,50 @@
 # shopping_cart.py
 
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+import os
+import pandas as pd
+import datetime
 
 def to_usd(my_price):
         return f"${my_price:,.2f}" #> $12,000.71
+
+CSV_FILENAME = "products.csv"
+csv_filepath = os.path.join("data",CSV_FILENAME)
+data = pd.read_csv(csv_filepath)
+data_dict = data.to_dict('records')
+products = list(data_dict)
+
+# based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 total_price = 0
 selected_ids = []
 
 while True:
-    selected_id = input("Please input a product identifier: ")
-    if selected_id == "DONE":
-        break
-    else:
-        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        #matching_product = matching_products[0]
-        #total_price = total_price + matching_product["price"]
-        #print(f"SELECTED PRODUCT: " + str(matching_product["name"]) + " " + str(to_usd(matching_product["price"])))
-        selected_ids.append(selected_id)
-
-
+      selected_id = input("Please input a product identifier: ")
+      if selected_id == "DONE":
+          break
+      else:        
+         selected_ids.append(selected_id)
 
 print("---------------------------------")
+print("GREEN FOODS GROCERY")
+print("WWW.GREEN-FOODS-GROCERY.COM")
+print("---------------------------------")
+today = datetime.datetime.now()
+print("CHECKOUT AT: " + today.strftime("%Y-%m-%d %I:%M %p"))
+print("SELECTED PRODUCTS:")
 for selected_id in selected_ids:
-    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-    matching_product = matching_products[0]
-    total_price = total_price + matching_product["price"]
-    print(f"SELECTED PRODUCT: " + str(matching_product["name"]) + " " + str(to_usd(matching_product["price"])))
-
-
-
-print(f"TOTAL PRICE: " + str(to_usd(total_price)))
+      matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+      matching_product = matching_products[0]
+      total_price = total_price + matching_product["price"]
+      print(f"... " + str(matching_product["name"]) + " " + str("(") + str(to_usd(matching_product["price"]) + str(")")))
+print("---------------------------------")
+print(f"SUBTOTAL: " + str(to_usd(total_price)))
+tax = total_price * 0.0875
+print("TAX: " + str(to_usd(tax)))
+print("TOTAL: " + str(to_usd(total_price + tax)))
+print("---------------------------------")
+print("THANKS, SEE YOU AGAIN SOON!")
+print("---------------------------------")
 
 # Example Output
 #(shopping-env)  --->> python shopping_cart.py
